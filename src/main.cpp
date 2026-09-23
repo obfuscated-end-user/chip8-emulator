488da1dfd534f3f76bfc14ac6b6a59ad7cd3ed4b
Yokohama <artuzcapati2@gmail.com>
2026-09-23 20:42:32 +0800

    added a subroutine

--- src/main.cpp
+++ src/main.cpp
@@ -156,6 +156,16 @@ public:
 		uint8_t Vy = (opcode & 0x00F0u) >> 4u;
 		if (registers[Vx] == registers[Vy]) pc += 2;
 	}
+	
+	void OP_6xkk() {
+		/**
+		 * LD Vx, byte
+		 * Set Vx = kk.
+		 */
+		uint8_t Vx (opcode & 0x0F00u) >> 8u;
+		uint8_t byte = opcode & 0x00FFu;
+		registers[Vx] = byte;
+	}
 };
 
 /**
